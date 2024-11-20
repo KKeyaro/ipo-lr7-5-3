b98f22b345a74cca798a2856374e656759774be7
Kkeyaro <Yaroshenkodmitrievna@gmail.com>
2024-11-20 19:18:36 +0300

    main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ while True:
     print("5. Выйти из программы")
     nomer = int(input("Выберете пункт: "))
     with open("city.json", "r", encoding = "utf-8") as file:
-        soderjim = file.read()
-        text = json.loads(soderjim)
+        text = json.load(file)
     if nomer == 1:
         count += 1
         for zapis in text:
@@ -27,34 +26,45 @@ while True:
                 print(f"Название города: {zapis['name']} \nНазвание страны в котором находится город: {zapis['country']} \nЯвляется ли население города больше 100 000 человек: {zapis['is_big']} \nНаселение города: {zapis['people_count']}")
                 found = True
                 break
-        if found == False:
+        if not found:
             print("Запись не найдена")
     elif nomer == 3:
-        count +=1
-        with open("city.json", "w", encoding = "utf-8") as file:
-            new = {}
-            new['d'] = input("Введите номер записи: ")
-            new['name'] = input("Введите название города: ")
-            new['country'] = input("Введите  название страны в котором находится город: ")
-            new['is_big'] = input("Является ли население города больше 100 000 человек(True/False): ")
-            new['people_count'] = input("Введите население города: ")
-            text.append(new)
-            print("Запись добавлена")
-            json.dump(text, file, indent = 5)
+        count += 1
+        new = {}
+        new['id'] = str(max(int(city['id']) for city in text) + 1 if text else 1)
+        new['name'] = input("Введите название города: ")
+        new['country'] = input("Введите название страны: ")
+            
+        while True:
+            people_count = input("Введите население города: ")
+            if people_count.isdigit():
+                new['people_count'] = int(people_count)
+                break
+            else:
+                print("Ошибка: население города должно быть числом.")
+                    
+        new['is_big'] = (True if new['people_count'] > 100000 else False)
+        text.append(new)
+            
+        with open("city.json", "w", encoding="utf-8") as file:
+            json.dump(text, file, indent=5)
+                
+        print("Запись добавлена")
+        
 
     elif nomer == 4:
         count += 1
-        nomer = input("Введите поле для удаления: ")
+        pole = input("Введите поле для удаления: ")
         found = False
         for city in text:
-            if nomer == city['id']:
+            if city['id'] == pole:
                 text.remove(city)
                 found = True
                 break
         if found:
             with open("city.json", "w", encoding = "utf-8") as file:
                 json.dump(text, file, indent = 5)
-                print("Запись удалена")
+            print("Запись удалена")
         else:
             print("Запись не найдена")
 
